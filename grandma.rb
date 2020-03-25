73cee69f79b7b2ab73ba63198fb4ddea0baa2127
Toni  Diep <adiep123@gmail.com>
2020-03-25 03:30:34 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,9 +15,9 @@
 def speak_to_grandma (phrase)
   if phrase = "I LOVE YOU GRANDMA!"
      return "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase.downcase == "I LOVE YOU GRANDMA!"
-     return "NO, NOT SINCE 1938!"
-  else
+  elsif phrase.upcase == "I LOVE YOU GRANDMA!"
      return "HUH?! SPEAK UP, SONNY!"
+  else
+     return "NO, NOT SINCE 1938!"
   end
 end
